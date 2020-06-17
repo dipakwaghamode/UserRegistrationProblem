3e2e67473e70a49b33bce9f390f847faf626b52c
sagarwaghamode <dipakwaghamode@gmail.com>
2020-06-17 14:16:29 +0530

    Updated UC-1 Requirment

--- userregistrationproblem.sh
+++ userregistrationproblem.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-
+#shot -s extglob
 echo " User Registration Problem "
+
+read -p "Enter user name:" username
+
+#if [[ $username =~ ^[A-Z}{1}[a-z]{2,}$ ]]
+
+#//if [[ $username =~ ^[A-Z][a-z]{2,}$ ]]
+
+if [[ $username =~ ^[[:upper:]]+([a-z]{3,})$ ]]
+
+then
+
+echo "User name is valid $username "
+
+else
+        echo "User name is not valid $username"
+fi
