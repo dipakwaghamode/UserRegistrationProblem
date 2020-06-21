815558e0ea0c329e29e4fc5e28d5e5cba6326620
sagarwaghamode <dipakwaghamode@gmail.com>
2020-06-21 21:01:51 +0530

    updated UC-5

--- userregistrationproblem.sh
+++ userregistrationproblem.sh
@@ -1,56 +1,16 @@
 #!/bin/bash
 
-read -p "Enter password" pwd
-        passpattern1="^([a-zA-Z0-9@#!]){8,}$"
-        passpattern2="^([a-z0-9@#!]*)[A-Z]+([a-z0-9@#!]*)$"
-        passpattern3="^[a-zA-Z@#!]*[0-9]+[a-zA-Z@#!]*$"
-        passpattern4="^([a-zA-Z0-9]*)[^a-zA-Z_0-9\s]([a-zA-Z0-9]*)$"
+read -p "Password should be minimum 8 characters"pwd
+
+passwordpattern="^[0-9a-zA-Z#@!]{8}$"
 
 
-if [[ $pwd =~ $passpattern1 ]]
-        then
-                if [[ $pwd =~ $passpattern2 ]]
-        then
-                if [[ $pwd =~ $passpattern3 ]]
-        then
-                if [[ $pwd =~ $passpattern4 ]]
-        then
-                echo "Sucess : valid password"
-        else
-                echo "Error : At least one character required"
-        fi
-        else
-                echo "Error : At least one digit manditory"
+if [[ pwd =~ $passwordpattern ]]
+then
 
-        fi
-        else
-                echo "Error: At least single cap is required"
-        fi
+	echo " Valid password"
 
-        else
-                echo "Error : password must be 8 length "
-        fi
-
-
-
-
-
-
-
-
-
-
-#read -p "Password should be minimum 8 characters"pwd
-
-#passwordpattern="^[a-zA-Z0-9@#!]){8,}$"
-
-
-#if [[ pwd =~ $passwordpattern ]]
-#then
-
-#	echo " Valid password"
-
-#else
-#        echo "Invalid Password"
-#fi
+else
+        echo "Invalid Password"
+fi
 
